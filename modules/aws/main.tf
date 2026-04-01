2269bc7fbd7ec14dbb4c1df1574c102ef219a2dc
Hoa, Tran Van <hoatrancntt@gmail.com>
2026-04-02 00:39:19 +0700

    Update main.tf

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -10,13 +10,20 @@ terraform {
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]
+
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
   }
 }
 
