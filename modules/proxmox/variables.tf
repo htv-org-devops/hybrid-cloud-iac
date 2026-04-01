3a46d74507edab697333e27d8837614902b4626a
Hoa, Tran Van <hoatrancntt@gmail.com>
2026-04-02 01:02:07 +0700

    Update variables.tf

--- modules/proxmox/variables.tf
+++ modules/proxmox/variables.tf
@@ -1,9 +1,43 @@
-variable "vmid"          { type = number; default = 8888 }
-variable "template_id"   { type = number; default = 9999 }
-variable "vm_name"       { type = string; default = "db-server" }
-variable "vm_ip"         { type = string; default = "172.199.10.180/24" }
-variable "gateway"       { type = string; default = "172.199.10.1" }
-variable "memory"        { type = number; default = 4096 }
-variable "disk_size"     { type = string; default = "30G" }
-variable "cores"         { type = number; default = 2 }
-variable "ssh_public_key"{ type = string }
+variable "vmid" {
+  type    = number
+  default = 8888
+}
+
+variable "template_id" {
+  type    = number
+  default = 9999
+}
+
+variable "vm_name" {
+  type    = string
+  default = "db-server"
+}
+
+variable "vm_ip" {
+  type    = string
+  default = "172.199.10.180/24"
+}
+
+variable "gateway" {
+  type    = string
+  default = "172.199.10.1"
+}
+
+variable "memory" {
+  type    = number
+  default = 4096
+}
+
+variable "disk_size" {
+  type    = string
+  default = "30G"
+}
+
+variable "cores" {
+  type    = number
+  default = 2
+}
+
+variable "ssh_public_key" {
+  type = string
+}
