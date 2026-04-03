082b35d3f736284a0c409bc1cc6e4e76a239603d
Hoa, Tran Van <hoatrancntt@gmail.com>
2026-04-03 14:58:45 +0700

    Update outputs.tf

--- modules/aws/outputs.tf
+++ modules/aws/outputs.tf
@@ -1,12 +1,19 @@
 output "alb_dns_name" {
-  value = aws_lb.web.dns_name
+  description = "ALB DNS name"
+  value       = aws_lb.web.dns_name
 }
+
 output "web_instance_ips" {
-  value = aws_instance.web[*].public_ip
+  description = "EC2 public IPs"
+  value       = aws_instance.web[*].public_ip
 }
+
 output "web_instance_private_ips" {
-  value = aws_instance.web[*].private_ip
+  description = "EC2 private IPs"
+  value       = aws_instance.web[*].private_ip
 }
+
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "VPC ID"
+  value       = aws_vpc.main.id
 }
