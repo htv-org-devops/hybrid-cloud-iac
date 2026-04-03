4a460b9923a75883997bdca046d2c144cd7a960d
Hoa, Tran Van <hoatrancntt@gmail.com>
2026-04-03 17:22:08 +0700

    Update outputs.tf

--- environments/proxmox-prod/outputs.tf
+++ environments/proxmox-prod/outputs.tf
@@ -1,2 +1,7 @@
-output "db_vm_ip"   { value = module.proxmox_db.db_vm_ip }
-output "db_vm_name" { value = module.proxmox_db.db_vm_name }
+output "db_vm_ip" {
+  value = module.proxmox_db.db_vm_ip
+}
+
+output "db_vm_name" {
+  value = module.proxmox_db.db_vm_name
+}
